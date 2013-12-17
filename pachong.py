920f989efd708c649ca3056af43aad05ea586656
xwc <1450033215@qq.com>
2013-12-17 20:06:24 +0800

    Update pachong.py

--- pachong.py
+++ pachong.py
@@ -5,9 +5,9 @@ import re                  //引入正则表达式
 import time                //引入时间函数的库
 from random import choice#用来随机选择代理ip
 iplist = ['114.32.36.229 8080','24.139.68.242 9090','119.115.136.226 80','219.150.117.116']
-mds=["中国","科技","北邮","数学","语文","英语","科学"]
+mds=["中国","科技","北邮","数学","语文","英语","科学"]  //建立一个列表，其中罗列一些关键字
 output=open('output.txt','a')
-for item in mds:
+for item in mds:      //对上述建立的列表中的元素进行遍历
     ip=choice(iplist)                 //从ip代理中选择一个ip
     timestamp = "%d" % time.time()
     gjc = urllib.quote(item)
@@ -20,7 +20,7 @@ for item in mds:
        "host":"suggestion.baidu.com",
        "Referer":"http://www.baidu.com/",
        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36",
-       }                              
+       }                              //网页中的header包含了一系列重要的参数，包扩url，代理，等等
     proxy_handler = urllib2.ProxyHandler({'http': 'http://'+ip})
     opener = urllib2.build_opener(proxy_handler)
     urllib2.install_opener(opener)
