4e390510c4f7ffdc4516f0d0ff370dba38ca47e6
xwc <1450033215@qq.com>
2013-12-17 19:21:40 +0800

    第一次更新

--- pachong.py
+++ pachong.py
@@ -1,26 +1,26 @@
 #coding:utf-8
-import urllib
-import urllib2
-import re
-import time
+import urllib         
+import urllib2             //python中固有的两个库
+import re                  //引入正则表达式
+import time                //引入时间函数的库
 from random import choice#用来随机选择代理ip
 iplist = ['114.32.36.229 8080','24.139.68.242 9090','119.115.136.226 80','219.150.117.116']
 mds=["中国","科技","北邮","数学","语文","英语","科学"]
 output=open('output.txt','a')
 for item in mds:
-    ip=choice(iplist)
+    ip=choice(iplist)                 //从ip代理中选择一个ip
     timestamp = "%d" % time.time()
     gjc = urllib.quote(item)
     #print timestamp  
     url = "http://suggestion.baidu.com/su?wd="+gjc+"&p=3&cb=window.bdsug.sug&sid=&t=" + timestamp
     #print url
-    time.sleep(2) 
+    time.sleep(2)                    //对爬虫的抓取速度进行限制
     header={
        "GET":url,
        "host":"suggestion.baidu.com",
        "Referer":"http://www.baidu.com/",
        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36",
-       }
+       }                              
     proxy_handler = urllib2.ProxyHandler({'http': 'http://'+ip})
     opener = urllib2.build_opener(proxy_handler)
     urllib2.install_opener(opener)
